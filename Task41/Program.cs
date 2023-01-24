ba95e7e8e695d094e65095a2c9984e82f713813a
Vikki <vikki_ka@mail.ru>
2023-01-24 21:30:15 +0300

    init repo

---
+++ Task41/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+int GetUserData(string massage)
+{
+    Console.ForegroundColor=ConsoleColor.DarkBlue;
+    Console.WriteLine(massage);
+    int UserData = int.Parse(Console.ReadLine()!);
+    Console.ResetColor();
+    return UserData;
+}
+void CountPositiveNumber (int length)
+{
+    int [] array = new int[length];
+    int count = 0;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = GetUserData("Ввидите число массива");
+        if (array[i] > 0 ) count++;
+    }
+    PrintArray(array);
+    Console.WriteLine($" -> Положительных чисел {count}");
+}
+void PrintArray(int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == arr.Length -1)
+        {
+            Console.Write(arr[i] + "");
+        }
+        else Console.Write(arr[i] + ", ");
+    }
+ 
+}
+
+int lengthArray = GetUserData("Ввидите длину массива: ");
+CountPositiveNumber(lengthArray);
---
+++ Task43/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+int GetUserData(string massage)
+{
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine(massage);
+    int UserData = int.Parse(Console.ReadLine()!);
+    Console.ResetColor();
+    return UserData;
+}
+void FindPointCrossLine(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Точка пересечения ({x}; {y})");
+}
+double b1 = GetUserData("Ввидите b1: ");
+double k1 = GetUserData("Ввидите k1: ");
+double b2 = GetUserData("Ввидите b2: ");
+double k2 = GetUserData("Ввидите k2: ");
+FindPointCrossLine(b1, k1, b2, k2);
